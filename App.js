3b169eaa564f5e7ea7b0808ab2cde2ed7e4849b0
WarrenWu4 <warrenweiwu04@gmail.com>
2022-10-05 21:07:45 -0500

    Created header

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //return for mobile development essentially same as ReactDOM.render() but for mobile components
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>TODO LIST PROTOTYPE</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  heading: {
+    color: '#000000',
+    fontSize: 30,
+    fontWeight: '600',
+    marginTop: 70,
+    marginBottom: 10,
+    marginLeft: 20,
   },
 });
