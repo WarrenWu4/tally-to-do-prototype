8c233a6dd41c8a6aae4e03bb4e758abddf63e858
WarrenWu4 <warrenweiwu04@gmail.com>
2022-10-07 08:00:37 -0500

    remove comment

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
 import TaskInput from 'to-do-prototype/TaskInput.js';
 import Task from 'to-do-prototype/Task.js';
 
-//testing commits and git
+//main app
 export default function App() {
   const [tasks, setTasks] = useState([]);
 
